15c1b466cad06657302bd257bd56a1eb51a9dae5
暮鼓晨钟 <>
2021-01-28 16:24:42 +0800

    add btn

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,16 +1,63 @@
 import React, { FC } from 'react';
+import classNames from 'classnames';
+import LoadingIcon from './LoadingIcon';
 
 export interface IButtonProp {
+  /**
+   * @description 类型
+   * @default 'default'
+   */
+  type?: 'primary' | 'link' | 'default'
+  /**
+   * @description 设为块标签 宽度为父级宽度
+   */
+  block?: boolean
+  /**
+   * @description 按钮失效状态
+   */
+  disabled?: boolean
+  /**
+   * @description 按钮载入状态
+   */
+  loading?: boolean
+  /**
+   * @description 按钮点击回调
+   */
+  onClick?: React.MouseEventHandler<HTMLElement>
+  /**
+   * @description class
+   */
+  className?: string
 }
 
-const PREFIX = 'leo-button';
+const PREFIX = 'leo-btn';
 
-const Button: FC<IButtonProp> = ({ children }) => {
+const Button: FC<IButtonProp> = (props) => {
+  const { children, type, loading, className, block, disabled, onClick }=props;
+  const classes = classNames(
+      PREFIX,
+      {
+        [`${PREFIX}-${type}`]: type,
+        [`${PREFIX}-block`]: block,
+      },
+      className,
+  );
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>) => {
+    if (loading) {
+      return;
+    }
+    if (onClick) {
+      (onClick as React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>)(e);
+    }
+  };
   return (
-    <div className={PREFIX}>
-      {children}
-    </div>
+    <button onClick={handleClick} disabled={disabled} className={classes}>
+      <LoadingIcon loading={loading} prefix={PREFIX}/>
+      <span>{children}</span>
+    </button>
   );
 };
 
+Button.defaultProps={
+};
 export default Button;
